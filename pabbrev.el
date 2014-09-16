b1c9a603ba693e2a9935730845493af8d0a9a6f5
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-09-16 22:34:48 +0100

    Version 4.0

--- pabbrev.el
+++ pabbrev.el
@@ -8,7 +8,7 @@
 
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
-;; Version: 3.2-alpha
+;; Version: 4.0
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
 ;;
