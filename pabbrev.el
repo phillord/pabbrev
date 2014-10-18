e0ac26a97fb255eec9f40366393ceb958f9af355
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-10-18 14:40:51 +0100

    Set version to 0 for ELPA.
    ELPA will not publish anything at all if the Version is set
    to 0, so do this until I am sure that I have everything in place.

--- pabbrev.el
+++ pabbrev.el
@@ -6,7 +6,7 @@
 
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
-;; Version: 4.1-alpha
+;; Version: 0
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
 ;;
