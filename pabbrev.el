a2a19c065d4ceb7fb4dc70577754271c0e9d1948
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-10-20 22:02:59 +0100

    Update Header comments
    Copyright to FSF, GPL to version 3.

--- pabbrev.el
+++ pabbrev.el
@@ -1,8 +1,6 @@
 ;;; pabbrev.el --- Predictive abbreviation expansion
 
-;; Copyright (C) 2003-2014 by Phillip Lord
-
-;; This file is not part of Emacs
+;; Copyright (C) 2003-2014 Free Software Foundation, Inc.
 
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
@@ -14,7 +12,7 @@
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; This program is distributed in the hope that it will be useful,
